839da6c39212c4ed6b89c956175954595de01aba
gnuga1 <115860949+gnuga1@users.noreply.github.com>
2022-10-22 19:14:11 +0700

    Update YellowFlowers.java

--- YellowFlowers.java
+++ YellowFlowers.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package polimorfismo;
-
-/**
- *
- * @author HP 14S
- */
 public class YellowFlowers extends Flowers{
     String type = "Yellow Flower";
     
